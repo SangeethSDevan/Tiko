09f88b43634a132d631325b46079af43fb1e0a67
Sangeeth S Devan <23dev500@gmail.com>
2025-10-24 21:14:41 +0530

    smtp fix

--- src/constants/transport.ts
+++ src/constants/transport.ts
@@ -6,13 +6,14 @@ import hbs, { type NodemailerExpressHandlebarsOptions } from "nodemailer-express
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-
 const handlebarOptions = {
   viewEngine: {
     extName: ".hbs",
