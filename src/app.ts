acddc5a20402617703814fb1a095d483ec62924b
Sangeeth S Devan <23dev500@gmail.com>
2025-10-24 14:30:12 +0530

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import userRouter from "./Routers/userRouter.js"
 import { authMiddleware } from "./Middlewares/auth.js"
 import eventRouter from "./Routers/eventRouter.js"
 import groupRouter from "./Routers/groupRouter.js"
+import cors from 'cors'
 import { sendNotifications } from "./Controllers/pushController.js"
 
 const app=express()
 
 app.use(express.json())
-
+app.use(cors({
+    origin:"*"
+}))
 app.use("/",express.static("public"))
 
 app.use("/api/v1/user",userRouter)
